1157df1c41de7fb7a36da3a9998a5a1d0923922b
snqwq <99054951+snqwq@users.noreply.github.com>
2023-09-03 13:02:40 -0500

    Update missle.py

--- missle.py
+++ missle.py
@@ -5,3 +5,36 @@ import random
 import time
 import threading
 
+#ANCHOR - Constants
+gravity = 1
+missilespeed = 1
+
+#ANCHOR - Variables
+
+points = 0
+
+#SECTION - Set up
+#ANCHOR - Setup the window
+window = pyglet.window.Window(1000, 750)
+window.set_caption("Missile sim")
+
+#ANCHOR - Setup batches
+entity_batch = pyglet.graphics.Batch()
+terrain_batch = pyglet.graphics.Batch()
+
+#ANCHOR - Setup missile
+missile = pyglet.sprite.Sprite(pyglet.image.load("assets/missile.png"), x=0, y=0, batch=entity_batch)
+missile.rotation = 90
+
+#ANCHOR - place missile
+missile.x = window.width / 2
+missile.y = window.height / 2
+
+
+@window.event
+def on_draw():
+    window.clear()
+    terrain_batch.draw()
+    entity_batch.draw()
+
+pyglet.app.run()
